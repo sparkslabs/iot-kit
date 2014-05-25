26cd48bcb849059919819e82ff7f55571907acae
Michael Sparks <sparks,m@gmail.com>
2014-05-25 20:55:46 +0100

    Initial python spike for stdio introspection mechanics

---
+++ Arduino/introspect_test.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import os, select, time
+
+class commandio(object):
+    def __init__(self, command):
+        self.command = command
+        self._send, self._recv = os.popen2(self.command)
+
+    def recv(self):
+        time.sleep(0.01)
+        r,w,e = select.select([self._recv], [self._send], [])
+        if self._recv not in r:
+            raise Exception("Bad Device")
+        line = self._recv.readline()
+        return line
+
+    def send(self, data):
+        self._send.write(data + "\n")
+        self._send.flush()
+
+io = commandio("./libraries/IOToy/examples/BotHostTiny/build-stdio/BotHostStdio")
+
+startline = io.recv()
+print startline
+
+for data in ["help", "help help", "funcs", "attrs"]:
+    io.send(data)
+    line = io.recv()
+    print data, (20-len(data))*" ", "--> :", repr(line)
