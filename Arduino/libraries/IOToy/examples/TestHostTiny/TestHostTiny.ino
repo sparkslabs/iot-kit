fb54d3d5d342e4bed4b45c1482c688ec5fd3ed38
Michael Sparks <sparks,m@gmail.com>
2014-05-27 20:55:38 +0100

    Change TestHostTiny to exercise system testing
    TestHostTiny now contains interesting things to introspect matching
    the API spec, but doesn't implement functionality. It's intended to
    provide a useful parsable data source for the python host code.

--- Arduino/libraries/IOToy/examples/TestHostTiny/TestHostTiny.ino
+++ Arduino/libraries/IOToy/examples/TestHostTiny/TestHostTiny.ino
@@ -2,21 +2,13 @@
 
 #include <CommandHostTiny.h>
 
-const int LEFT_MOTOR_DIR_PIN = 7;
-const int LEFT_MOTOR_PWM_PIN = 9;
-const int RIGHT_MOTOR_DIR_PIN = 8;
-const int RIGHT_MOTOR_PWM_PIN = 10;
-
-const int LED_PIN = 13;
-
 const int DRIVE_FORWARD_TIME_MS = 1500;
 const int TURN_TIME_MS = 2000;
 
-
 class TestHost : public CommandHostTiny {
 private:
-  int drive_forward_time_ms; // Forward distance
-  int turn_time_ms; // 
+  int drive_forward_time_ms; // To be replaced
+  int turn_time_ms;          // To be replaced
   char temp_str[128];
 public:
   TestHost() : drive_forward_time_ms(DRIVE_FORWARD_TIME_MS), turn_time_ms(TURN_TIME_MS) { }
@@ -30,31 +22,48 @@ public:
     return "drive_forward_time_ms:int,turn_time_ms:int";
   }
 
-  const char * funcs() { // need to implement on/off(!)
-    return "forward,backward,left,right,on,off";
+  const char * funcs() {
+    return "barecommand,one_arg_int,one_arg_bool,one_arg_str,one_arg_float,one_arg_T,no_arg_result_int,no_arg_result_bool,no_arg_result_str,no_arg_result_float,no_arg_result_T,one_arg_int_result_int";
   }
 
   bool has_help(char * name) {
     if (strcmp(name,"drive_forward_time_ms")==0) return true;
     if (strcmp(name,"turn_time_ms")==0) return true;
-    if (strcmp(name,"forward")==0) return true;
-    if (strcmp(name,"backward")==0) return true;
-    if (strcmp(name,"left")==0) return true;
-    if (strcmp(name,"right")==0) return true;
-    if (strcmp(name,"on")==0) return true;
-    if (strcmp(name,"off")==0) return true;
+    if (strcmp(name,"one_arg_int")==0) return true;
+    if (strcmp(name,"one_arg_bool")==0) return true;
+    if (strcmp(name,"one_arg_str")==0) return true;
+    if (strcmp(name,"one_arg_float")==0) return true;
+    if (strcmp(name,"one_arg_T")==0) return true;
+    if (strcmp(name,"barecommand")==0) return true;
+
+    if (strcmp(name,"no_arg_result_int")==0) return true;
+    if (strcmp(name,"no_arg_result_bool")==0) return true;
+    if (strcmp(name,"no_arg_result_str")==0) return true;
+    if (strcmp(name,"no_arg_result_float")==0) return true;
+    if (strcmp(name,"no_arg_result_T")==0) return true;
+
+    if (strcmp(name,"one_arg_int_result_int")==0) return true;
+
     return false;
   }
 
   const char *help(char * name) {
     if (strcmp(name,"drive_forward_time_ms")==0) return "int - How long to move forward";
     if (strcmp(name,"turn_time_ms")==0) return "int - How long to turn";
-    if (strcmp(name,"forward")==0) return "forward distance:int -> - Move forward for drive_forward_time_ms milliseconds";
-    if (strcmp(name,"backward")==0) return "backward distance:int -> - Move backward for drive_forward_time_ms milliseconds";
-    if (strcmp(name,"left")==0) return "left angle:int -> - Turn left for turn_time_ms milliseconds";
-    if (strcmp(name,"right")==0) return "right angle:int -> - Turn right for turn_time_ms milliseconds";
-    if (strcmp(name,"on")==0) return "on -> result:bool - Turn on";
-    if (strcmp(name,"off")==0) return "off -> result:bool - Turn off";
+    if (strcmp(name,"barecommand")==0) return "barecommand -> - test, basic command, no arg/result";
+    if (strcmp(name,"one_arg_int")==0) return "one_arg_int myarg:int -> - test, one arg, integer";
+    if (strcmp(name,"one_arg_bool")==0) return "one_arg_bool myarg:bool -> - test, one arg, boolean";
+    if (strcmp(name,"one_arg_str")==0) return "one_arg_str myarg:str -> - test, one arg, string";
+    if (strcmp(name,"one_arg_float")==0) return "one_arg_float myarg:float -> - test, one arg, float";
+    if (strcmp(name,"one_arg_T")==0) return "one_arg_T attr:T -> - test, one arg, generic type";
+
+    if (strcmp(name,"no_arg_result_int")==0) return "no_arg_result_int -> result:int - test, one arg, integer";
+    if (strcmp(name,"no_arg_result_bool")==0) return "no_arg_result_bool -> result:bool - test, one arg, boolean";
+    if (strcmp(name,"no_arg_result_str")==0) return "no_arg_result_str -> result:str - test, one arg, string";
+    if (strcmp(name,"no_arg_result_float")==0) return "no_arg_result_float -> result:float - test, one arg, float";
+    if (strcmp(name,"no_arg_result_T")==0) return "no_arg_result_T -> result:T - test, one arg, generic type";
+
+    if (strcmp(name,"one_arg_int_result_int")==0) return "one_arg_int_result_int -> result:int - test, one arg, one result, both ints";
     return "-";
   }
 
@@ -91,64 +100,13 @@ public:
   }
 
   int callfunc(char* funcname, char* raw_args) { 
-    if (strcmp(funcname,"forward")==0) { forward(); return 200; }
-    if (strcmp(funcname,"backward")==0) { backward(); return 200; }
-    if (strcmp(funcname,"left")==0) { left(); return 200; }
-    if (strcmp(funcname,"right")==0) { right(); return 200; }
-    return 404; 
-  }
-
-  void stop(void) {
-    analogWrite( LEFT_MOTOR_PWM_PIN, 0 );
-    analogWrite( RIGHT_MOTOR_PWM_PIN, 0 );
-  }
-  void forward(void) {
-    digitalWrite(LED_PIN, HIGH);   // Turn LED on while going forward
-    digitalWrite( LEFT_MOTOR_DIR_PIN, HIGH );
-    digitalWrite( RIGHT_MOTOR_DIR_PIN, HIGH );
-    analogWrite( LEFT_MOTOR_PWM_PIN, 255 );
-    analogWrite( RIGHT_MOTOR_PWM_PIN, 255 );
-    delay( drive_forward_time_ms );
-    stop();
-  }
-  void backward(void) {
-    digitalWrite(LED_PIN, HIGH);   // Turn LED on while going forward
-    digitalWrite( LEFT_MOTOR_DIR_PIN, LOW );
-    digitalWrite( RIGHT_MOTOR_DIR_PIN, LOW );
-    analogWrite( LEFT_MOTOR_PWM_PIN, 255 );
-    analogWrite( RIGHT_MOTOR_PWM_PIN, 255 );
-    delay( drive_forward_time_ms );
-    stop();
-  }
-  void right(void) {
-    digitalWrite(LED_PIN, LOW);   // Turn LED off while turning
-    digitalWrite( LEFT_MOTOR_DIR_PIN, HIGH );
-    digitalWrite( RIGHT_MOTOR_DIR_PIN, LOW );
-    analogWrite( LEFT_MOTOR_PWM_PIN, 255 );
-    analogWrite( RIGHT_MOTOR_PWM_PIN, 255 );
-    delay( turn_time_ms );
-    stop();
-  }
-  void left(void) {
-    digitalWrite(LED_PIN, LOW);   // Turn LED off while turning
-    digitalWrite( LEFT_MOTOR_DIR_PIN, LOW );
-    digitalWrite( RIGHT_MOTOR_DIR_PIN, HIGH );
-    analogWrite( LEFT_MOTOR_PWM_PIN, 255 );
-    analogWrite( RIGHT_MOTOR_PWM_PIN, 255 );
-    delay( turn_time_ms );
-    stop();
+    // Since this is a test host, it doesn't actually do anything
+    return 200;
   }
 
   void setup(void) {
       // Setup the pins
       CommandHostTiny::setup();
-
-      pinMode( LEFT_MOTOR_DIR_PIN, OUTPUT );
-      pinMode( LEFT_MOTOR_PWM_PIN, OUTPUT );
-      pinMode( RIGHT_MOTOR_DIR_PIN, OUTPUT );
-      pinMode( RIGHT_MOTOR_PWM_PIN, OUTPUT );
-
-      pinMode(LED_PIN, OUTPUT);
   }
 };
 
