bf75e72af6333cffeba1f9f9411dd59c3875b2e3
Michael Sparks <sparks,m@gmail.com>
2014-08-07 13:38:27 +0100

    Initial webhost for a device.
    Works pretty much out the box, just replaced commandio with serial_io,
    and removed references to checking for a commandline thing.

---
+++ Python/examples/webhost_for_arduino_device.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+"""
+
+Create a web interface for a stdio based command host - examples:
+
+    * ./webhost_for_device.py ../../Arduino/libraries/IOToy/examples/BotHostTiny/build-stdio/BotHostStdio
+    * ./webhost_for_device.py ../../Arduino/libraries/IOToy/examples/TestHostTiny/build-stdio/TestHostStdio
+
+"""
+import os
+import sys
+import pprint
+import json
+import time
+
+from iotoy.deviceproxy import serial_io, DeviceProxy
+from iotoy.webinterface import webserver_started, DeviceWebInterface
+from iotoy.discovery import IOTWebService
+
+io = serial_io("/dev/ttyUSB2", 19200, debug=False)
+
+proxy = DeviceProxy(device=io)
+proxy.introspect_device()
+
+dev_interface = DeviceWebInterface(proxy)
+dev_interface.start()
+
+dev_interface.wait_server_start()
+iotservice = IOTWebService(name=proxy.name)
+iotservice.start()
+iotservice.wait_advertised()
+
+print "Device ready - look for %s._iotoy._tcp.local via avahi-browse -r -a" % proxy.name
+
+# Wait for the web interface thread to exit
+while True:
+    time.sleep(1)
